84cca3d8661611b43a832380214f936bcf92f55c
guoxj9966 <guo_xue_jing@163.com>
2019-04-11 09:23:49 +0800

    删除标签

--- demo/demolist/Demo4.js
+++ demo/demolist/Demo4.js
@@ -7,13 +7,22 @@
 import React, { Component } from 'react';
 import Tag from '../../src';
 class Demo4 extends Component {
+    state = {
+        tags: ['员工编号','员工姓名','员工性别','所属部门']
+      };
+    
+    handleClose = (removedTag) => {
+        const tags = this.state.tags.filter(tag => tag !== removedTag);
+        console.log(tags);
+        this.setState({ tags });
+    }
     render () { 
         return (
             <div className="demoPadding" >
-                <Tag  visible={true} deleted>员工编号</Tag>
-                <Tag  visible={true} deleted>员工姓名</Tag>
-                <Tag  visible={true} deleted>员工性别</Tag>
-                <Tag  visible={true} deleted>所属部门</Tag>
+                <Tag  visible={true} deleted onClose={this.handleClose}>员工编号</Tag>
+                <Tag  visible={true} deleted onClose={this.handleClose}>员工姓名</Tag>
+                <Tag  visible={true} deleted onClose={this.handleClose}>员工性别</Tag>
+                <Tag  visible={true} deleted onClose={this.handleClose}>所属部门</Tag>
             </div>
         )
     }
